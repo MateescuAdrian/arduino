9480fbb234bf617bfc27898acac4d62b3b8b1c60
Mateescu Adrian <adrian.mateescu2012@gmail.com>
2026-03-02 17:57:07 +0100

    made the servo steer work in mrauW

--- mrauW/mrauW.ino
+++ mrauW/mrauW.ino
@@ -6,9 +6,12 @@
 
 // Pins for H-Bridge
 const int IN1 = 36; 
-const int IN2 = 37; 
+const int IN2 = 37;
 const int IN3 = 38;
 const int IN4 = 39; 
+
+// Servo pin - Use GP17 (Physical Pin 12)
+const int SERVO_PIN = 17; 
 
 // PWM Settings
 const int freq = 5000;
@@ -23,11 +26,18 @@ void setup() {
   Serial.begin(115200);
   delay(3000); 
 
-  // Attach PWM to all pins
+  // Configure Motor Pins for PWM
   ledcAttach(IN1, freq, res);
   ledcAttach(IN2, freq, res);
   ledcAttach(IN3, freq, res);
   ledcAttach(IN4, freq, res);
+  
+  // Configure Servo Signal using 50Hz for standard servos
+  ledcAttach(SERVO_PIN, 50, 10); // 10-bit resolution (0-1023)
+  
+  // Initial center position (90 degrees)
+  int centerDuty = map(90, 0, 180, 26, 123);
+  ledcWrite(SERVO_PIN, centerDuty);
   
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -37,35 +47,41 @@ void setup() {
   WiFi.setSleep(false); 
 
   WebSerial.begin(&server);
+  Serial.println(WiFi.localIP());
 
-  // SINGLE ROUTE FOR BOTH MOTORS
+  // UPDATED ROUTE: /drive?m1=XX&servo=YY
   server.on("/drive", HTTP_GET, [](AsyncWebServerRequest *request) {
     String response = "Commands Processed: ";
 
-    // Handle Motor 1 (Parameter 'm1')
+    // 1. Single Parameter 'm1' controls BOTH motors
     if (request->hasParam("m1")) {
-      int s1 = constrain(request->getParam("m1")->value().toInt(), -255, 255);
-      if (s1 >= 0) {
-        ledcWrite(IN1, s1);
+      int speedValue = constrain(request->getParam("m1")->value().toInt(), -255, 255);
+      
+      if (speedValue >= 0) {
+        // Forward: Both motors use high signal on IN1 and IN3
+        ledcWrite(IN1, speedValue);
         ledcWrite(IN2, 0);
-      } else {
-        ledcWrite(IN1, 0);
-        ledcWrite(IN2, abs(s1));
-      }
-      response += "M1=" + String(s1) + " ";
-    }
-
-    // Handle Motor 2 (Parameter 'm2')
-    if (request->hasParam("m2")) {
-      int s2 = constrain(request->getParam("m2")->value().toInt(), -255, 255);
-      if (s2 >= 0) {
-        ledcWrite(IN3, s2);
+        ledcWrite(IN3, speedValue);
         ledcWrite(IN4, 0);
       } else {
+        // Backward: Both motors use high signal on IN2 and IN4
+        ledcWrite(IN1, 0);
+        ledcWrite(IN2, abs(speedValue));
         ledcWrite(IN3, 0);
-        ledcWrite(IN4, abs(s2));
+        ledcWrite(IN4, abs(speedValue));
       }
-      response += "M2=" + String(s2);
+      response += "Motors=" + String(speedValue) + " ";
+    }
+
+    // 2. Add steering servo control
+    if (request->hasParam("servo")) {
+      int servoAngle = constrain(request->getParam("servo")->value().toInt(), 0, 180);
+      
+      // Map 0-180 angle to standard 50Hz servo duty cycle
+      int duty = map(servoAngle, 0, 180, 26, 123);
+      ledcWrite(SERVO_PIN, duty);
+      
+      response += "Servo=" + String(servoAngle);
     }
 
     WebSerial.println(response);
@@ -73,7 +89,7 @@ void setup() {
   });
 
   server.begin();
-  Serial.println("\nSUCCESS: Combined Drive Route Live!");
+  Serial.println("\nSUCCESS: All-in-One Route Live!");
 }
 
 void loop() {
